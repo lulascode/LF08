6b90402b959a03f8dc33bebe81194b8779424220
Frexax <Raykmoeker@web.de>
2026-03-30 10:18:19 +0200

    first draft

--- backend/tbd/DB-erstellungs-Skript.sql
+++ backend/tbd/DB-erstellungs-Skript.sql
@@ -1,1 +1,25 @@
+CREATE Database Pig_Racing;
 
+use Pig_Racing;
+
+CREATE TABLE video (
+	VideoID INT AUTO_INCREMENT PRIMARY KEY,
+	URL VARCHAR(256) NOT NULL 
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE Rennen (
+	RennenID INT AUTO_INCREMENT PRIMARY KEY,
+	Beginnzeit FLOAT NOT NULL,
+	Endzeit FLOAT NOT NULL,
+	Gewinner VARCHAR(128) NOT NULL
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE Schweine (
+	PigID Int AUTO_INCREMENT PRIMARY KEY,
+	Name Varchar(128) NOT NULL,
+	Beschleunigung INT NOT NULL, 
+	Maximalgeschwindigkeit INT NOT NULL, 
+	Gelaende_faehigkeit INT NOT NULL,
+	Handhabung INT NOT NULL,
+	Sprungkraft INT NOT NULL
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
